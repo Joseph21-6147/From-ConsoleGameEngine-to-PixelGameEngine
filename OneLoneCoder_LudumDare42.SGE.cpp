7f28b0105c9d15f9e1ae79d4104a16cb7d13b277
Joseph21 <93709765+Joseph21-6147@users.noreply.github.com>
2025-03-30 21:07:31 +0200

    Add files via upload

---
+++ OneLoneCoder_LudumDare42.SGE.cpp
@@ -0,0 +1,460 @@
+/*
+OneLoneCoder.com - Ludum Dare 42 - Running Out Of Space
+"What's all this about Fish?" - @Javidx9
+
+License
+~~~~~~~
+One Lone Coder Console Game Engine  Copyright (C) 2018  Javidx9
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions; See license for details.
+Original works located at:
+https://www.github.com/onelonecoder
+https://www.onelonecoder.com
+https://www.youtube.com/javidx9
+GNU GPLv3
+https://github.com/OneLoneCoder/videos/blob/master/LICENSE
+
+From Javidx9 :)
+~~~~~~~~~~~~~~~
+Hello! Ultimately I don't care what you use this for. It's intended to be
+educational, and perhaps to the oddly minded - a little bit of fun.
+Please hack this, change it and use it in any way you see fit. You acknowledge
+that I am not responsible for anything bad that happens as a result of
+your actions. However this code is protected by GNU GPLv3, see the license in the
+github repo. This means you must attribute me if you use it. You can view this
+license here: https://github.com/OneLoneCoder/videos/blob/master/LICENSE
+Cheers!
+
+Background
+~~~~~~~~~~
+Ludum Dare 42 - A quick and lazy speedcode following the theme of "Running Out Of Space"
+
+OBJECTIVE: Stay Alive! Use Arrow Keys to move player. Press space to deploy bombs
+to blow up encroaching walls. Collect bombs when they appear. Stay away from the
+blasts, and dont get trapped in the walls!
+
+
+Video
+~~~~~
+https://www.twitch.tv/videos/295730019
+
+Author
+~~~~~~
+Twitter: @javidx9
+Blog: http://www.onelonecoder.com
+Discord: https://discord.gg/WhwHUMV
+
+
+Last Updated: 11/08/2018
+*/
+
+/* Alterations Joseph21 - 2022-11-18
+ *   - removed "using namespace std" and added explicit scope resolution operator where needed
+ *   - had to make 0.0f an explicit float in call to std::max() (line 372)
+ *   - pixel size 10 instead of 14 to fit the window in my screen (line 454)
+ * Port to PixelGameEngine - 2022-11-18
+ *   - see cheat sheet for generic adaptations needed for port
+ *   - removed differentiated include of CGE header, since PGE is multiplatform
+ *   - used a call to DrawString() with string "P" instead call to Draw() with glyph 'P' to draw player
+ *   - because in the CGE each character is a glyph, and equal to 1x1 pixel in size, and in PGE the characters are
+ *     8x8 pixels in size, I introduced concept of "game pixel space", by defining constant GAME_PIXEL to be 8, and two
+ *     functions GPS_Draw() and GPS_DrawString() that have the same signature as the original Draw() and DrawString()
+ *     but also perform the transformation from game (world) size to game pixel size.
+ *   - adapted pixel size and window size in the call to Construct() to accomodate game pixel space
+ */
+
+#include "SGE/SGE_Core.h"
+
+#include <algorithm>
+#include <string>
+#include <list>
+
+//using namespace std;
+
+#define GAME_PIXEL 8     // to compensate for the character size
+
+class LudumDare42 : public flc::SDL_GameEngine
+{
+public:
+	LudumDare42()
+	{
+		sAppName = "Ludum Dare 42 - Running Out Of Space";
+	}
+
+
+private:
+	int nWorldWidth = 60;
+	int nWorldHeight = 60;
+
+	int *world = nullptr;
+
+	int nBlocksPlaced = 0;
+
+	float fBlockTimeElapsed = 0.0f;
+	float fBlockTime = 0.02f;
+
+	int nPlayerX;
+	int nPlayerY;
+
+	float fSurvivalTime = 0.0f;
+
+	enum
+	{
+		EMPTY,
+		BORDER,
+		BLOCK,
+	};
+
+
+	struct sParticle
+	{
+		float px, py;
+		float vx, vy;
+		float lifetime;
+		bool dead = false;
+	};
+
+	struct sBomb
+	{
+		int px, py;
+		float fuse;
+	};
+
+	std::list<sParticle> listParticles;
+	std::list<sBomb> listBombs;
+
+	int  nBombPickupX = 20;
+	int  nBombPickupY = 20;
+	bool bBombsVisible = true;
+	int  nBoomsticks = 1;
+
+	int nPlayerHealth = 100;
+	float fHighScore = 0.0f;
+
+	bool bReset = true;
+
+	// wrappers to convert to pixel game space
+	void GPS_Draw( int x, int y, flc::Pixel colour ) {
+        FillRect( x * GAME_PIXEL, y * GAME_PIXEL, GAME_PIXEL, GAME_PIXEL, colour );
+	}
+	void GPS_DrawString( int x, int y, std::string s, flc::Pixel colour = flc::WHITE ) {
+        DrawString( x * GAME_PIXEL, y * GAME_PIXEL, s, colour );
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		world = new int[nWorldWidth * nWorldHeight]{ 0 };
+		bReset = true;
+		return true;
+	}
+
+	void Boom(int xc, int yc, int r)
+	{
+		// Taken from wikipedia
+		int x = 0;
+		int y = r;
+		int p = 3 - 2 * r;
+		if (!r) return;
+
+		auto drawline = [&](int sx, int ex, int ny)
+		{
+			if (ny <= 0 || ny >= nWorldHeight) return;
+			sx = std::max(sx, 0);
+			ex = std::min(ex, nWorldWidth);
+			for (int i = sx; i <= ex; i++)
+			{
+
+				if (world[ny * nWorldWidth + i] != BORDER)
+				{
+					world[ny * nWorldWidth + i] = 0;
+					nBlocksPlaced--;
+				}
+			}
+		};
+
+		while (y >= x)
+		{
+			// Modified to draw scan-lines instead of edges
+			drawline(xc - x, xc + x, yc - y);
+			drawline(xc - y, xc + y, yc - x);
+			drawline(xc - x, xc + x, yc + y);
+			drawline(xc - y, xc + y, yc + x);
+			if (p < 0) p += 4 * x++ + 6;
+			else p += 4 * (x++ - y--) + 10;
+		}
+	};
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		if (bReset)
+		{
+			for (int x = 0; x<nWorldWidth; x++)
+				for (int y = 0; y < nWorldHeight; y++)
+				{
+					if (x == 0 || y == 0 || x == nWorldWidth - 1 || y == nWorldHeight - 1)
+						world[y*nWorldWidth + x] = BORDER;
+					else
+						world[y*nWorldWidth + x] = EMPTY;
+				}
+
+			nPlayerX = nWorldWidth / 2;
+			nPlayerY = nWorldHeight / 2;
+			nPlayerHealth = 100;
+			fSurvivalTime = 0;
+			nBombPickupX = 20;
+			nBombPickupY = 20;
+			bBombsVisible = true;
+			nBoomsticks = 1;
+			listBombs.clear();
+			listParticles.clear();
+			bReset = false;
+		}
+
+		if (nPlayerHealth <= 0)
+		{
+			// Show Game Over
+
+			// Clear Screen
+			FillRect(0, 0, ScreenWidth(), ScreenHeight(), flc::BLACK);
+			GPS_DrawString(10, 10, "Oh Dear! You ran out of space...");
+			GPS_DrawString(10, 12, "Your Score: " + std::to_string((int)fSurvivalTime));
+			GPS_DrawString(10, 14, "High Score: " + std::to_string((int)fHighScore));
+			GPS_DrawString(10, 20, "OneLoneCoder - www.youtube.com/javidx9");
+			GPS_DrawString(10, 22, "Ludum Dare 42 - 'Running Out Of Space'");
+			GPS_DrawString(10, 30, "Press 'R' to restart");
+
+			if (GetKey(flc::Key::R).bReleased)
+				bReset = true;
+
+			return true;
+		}
+
+		// Place Block, only on borders, or attached to
+		// neighbouring blocks
+
+		auto Get = [&](int x, int y)
+		{
+			return world[y*nWorldWidth + x];
+		};
+
+		auto Set = [&](int x, int y, int i)
+		{
+			world[y*nWorldWidth + x] = i;
+		};
+
+		fSurvivalTime += fElapsedTime;
+
+		fBlockTimeElapsed += fElapsedTime;
+		if (fBlockTimeElapsed >= fBlockTime)
+		{
+			fBlockTimeElapsed -= fBlockTime;
+			// TODO: perfect !
+			// May come back to this later to make it optimal
+			if (nBlocksPlaced < (nWorldWidth * nWorldHeight) - 2 * (nWorldWidth - 2) - 2 * (nWorldHeight - 2) - 4) // hmmm maybe? :D
+			{
+				bool bBlockPlaced = false;
+				while (!bBlockPlaced)
+				{
+					int x = (rand() % (nWorldWidth - 2) + 1);
+					int y = (rand() % (nWorldHeight - 2) + 1);
+
+					// Check if location is suitable, it is
+					// if neighbouring cell is not empty
+
+					if (!Get(x, y) &&
+						(Get(x - 1, y) || Get(x + 1, y) || Get(x, y - 1) || Get(x, y + 1)))
+					{
+						Set(x, y, BLOCK);
+						bBlockPlaced = true;
+						nBlocksPlaced++;
+					}
+				}
+			}
+
+			if (!bBombsVisible && nBoomsticks == 0)
+			{
+				bool bBlockPlaced = false;
+				while (!bBlockPlaced)
+				{
+					int x = (rand() % (nWorldWidth - 2) + 1);
+					int y = (rand() % (nWorldHeight - 2) + 1);
+					if (!Get(x, y))
+					{
+						bBlockPlaced = true;
+						nBombPickupX = x;
+						nBombPickupY = y;
+						bBombsVisible = true;
+					}
+				}
+			}
+
+			// Handle Player
+			if (IsFocused())
+			{
+				if (GetKey(flc::Key::UP).bHeld)
+				{
+					if (!Get(nPlayerX, nPlayerY - 1))
+						nPlayerY--;
+				}
+
+				if (GetKey(flc::Key::DOWN).bHeld)
+				{
+					if (!Get(nPlayerX, nPlayerY + 1))
+						nPlayerY++;
+				}
+
+				if (GetKey(flc::Key::LEFT).bHeld)
+				{
+					if (!Get(nPlayerX - 1, nPlayerY))
+						nPlayerX--;
+				}
+
+				if (GetKey(flc::Key::RIGHT).bHeld)
+				{
+					if (!Get(nPlayerX + 1, nPlayerY))
+						nPlayerX++;
+				}
+
+				if (bBombsVisible && (pow(nPlayerX-nBombPickupX,2) + pow(nPlayerY-nBombPickupY, 2)) <= 6)
+				{
+					bBombsVisible = false;
+					nBoomsticks += 5;
+				}
+
+				// Check if player is trapped
+				if (Get(nPlayerX - 1, nPlayerY) && Get(nPlayerX + 1, nPlayerY) &&
+					Get(nPlayerX, nPlayerY - 1) && Get(nPlayerX, nPlayerY + 1))
+				{
+					if (nBoomsticks == 0)
+						nPlayerHealth = 0;
+				}
+			}
+		}
+
+		// Update Particles
+		for (auto &p : listParticles)
+		{
+			p.vy += 100.0f * fElapsedTime;
+
+			p.px += p.vx * fElapsedTime;
+			p.py += p.vy * fElapsedTime;
+
+			if (p.px < 0 || p.px >= nWorldWidth || p.py < 0 || p.py >= nWorldHeight)
+			{
+				p.dead = true;
+			}
+		}
+
+		listParticles.remove_if([&](const sParticle &p) { return p.dead; });
+
+		// Update Bombs
+		for (auto &b : listBombs)
+		{
+			b.fuse -= fElapsedTime;
+			if (b.fuse < 0)
+			{
+				// Boom! if boomsticks are available
+				Boom(b.px, b.py, 5);
+
+				// Some sort of visual effect for explosion
+				for (int i = 0; i < 20; i++)
+				{
+					sParticle p;
+					p.px = b.px + (2.5f * (((float)rand() / (float)RAND_MAX) * 2.0f - 1.0f));
+					p.py = b.py + (2.5f * (((float)rand() / (float)RAND_MAX) * 2.0f - 1.0f));
+					p.vx = 1.1f * 50.0f * (((float)rand() / (float)RAND_MAX) * 2.0f - 1.0f);
+					p.vy = 1.1f * 50.0f * (((float)rand() / (float)RAND_MAX) * 2.0f - 1.0f);
+					p.dead = false;
+
+					listParticles.push_back(p);
+				}
+
+				// Injure player
+				nPlayerHealth -= std::max(0.0f, 4 * (5 - sqrtf(pow(nPlayerX - b.px, 2) + pow(nPlayerY - b.py, 2))));
+
+			}
+		}
+
+		listBombs.remove_if([&](const sBomb &b) { return b.fuse < 0; });
+
+		if (IsFocused() && GetKey(flc::Key::SPACE).bReleased)
+		{
+			if (nBoomsticks > 0)
+			{
+				listBombs.push_back({ nPlayerX, nPlayerY, 5.0f });
+				nBoomsticks--;
+			}
+		}
+
+		// Clear Screen
+		FillRect(0, 0, ScreenWidth(), ScreenHeight(), flc::BLACK);
+
+		// Draw World
+		for (int x = 0; x < nWorldWidth; x++)
+		{
+			for (int y = 0; y < nWorldHeight; y++)
+			{
+				switch (world[y*nWorldWidth + x])
+				{
+				case BORDER:
+					GPS_Draw(x, y, flc::WHITE);
+					break;
+
+				case BLOCK:
+					GPS_Draw(x, y, flc::RED);
+					break;
+				}
+			}
+		}
+
+		// Draw Boom
+		for (auto &p : listParticles)
+		{
+			GPS_Draw(p.px, p.py, flc::DARK_YELLOW);
+		}
+
+		// Draw Bombs
+		for (auto &b : listBombs)
+		{
+			GPS_Draw(b.px - 1, b.py, flc::GREEN);
+			GPS_Draw(b.px + 1, b.py, flc::GREEN);
+			GPS_Draw(b.px, b.py - 1, flc::GREEN);
+			GPS_Draw(b.px, b.py + 1, flc::GREEN);
+
+			GPS_DrawString(b.px, b.py, std::to_string((int)b.fuse + 1));
+		}
+
+		// Draw Bomb Pickup
+		if (bBombsVisible)
+		{
+			FillRect( (nBombPickupX - 1) * GAME_PIXEL, (nBombPickupY - 1) * GAME_PIXEL, 3 * GAME_PIXEL, 3 * GAME_PIXEL, flc::MAGENTA);
+			GPS_Draw(nBombPickupX, nBombPickupY, flc::GREY);
+		}
+
+
+		fHighScore = std::max(fHighScore, fSurvivalTime);
+
+		// Draw Player
+		GPS_DrawString(nPlayerX, nPlayerY, "P", flc::CYAN);
+
+		GPS_DrawString( 2, (ScreenHeight() - 10) / GAME_PIXEL, "Survived: " + std::to_string((int)fSurvivalTime));
+		GPS_DrawString(18, (ScreenHeight() - 10) / GAME_PIXEL, "Bombs: " + std::to_string(nBoomsticks));
+		GPS_DrawString(30, (ScreenHeight() - 10) / GAME_PIXEL, "Health: " + std::to_string(nPlayerHealth));
+		GPS_DrawString(45, (ScreenHeight() - 10) / GAME_PIXEL, "High Score: " + std::to_string((int)fHighScore));
+
+		return true;
+	}
+
+};
+
+
+int main( int argc, char *argv[] )
+{
+	LudumDare42 demo;
+	if (demo.Construct(60 * GAME_PIXEL, 63 * GAME_PIXEL, 1, 1))
+		demo.Start();
+	return 0;
+}
+
---
+++ OneLoneCoder_MatchingGems_50KSubSpecial.SGE.cpp
@@ -0,0 +1,476 @@
+/*
+	Live 50K Sub Special - Matching Gems Game
+	"Thanks everyone, it means so much..." - javidx9
+
+	License (OLC-3)
+	~~~~~~~~~~~~~~~
+
+	Copyright 2018-2019 OneLoneCoder.com
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions
+	are met:
+
+	1. Redistributions or derivations of source code must retain the above
+	copyright notice, this list of conditions and the following disclaimer.
+
+	2. Redistributions or derivative works in binary form must reproduce
+	the above copyright notice. This list of conditions and the following
+	disclaimer must be reproduced in the documentation and/or other
+	materials provided with the distribution.
+
+	3. Neither the name of the copyright holder nor the names of its
+	contributors may be used to endorse or promote products derived
+	from this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+	A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+	THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	Relevant Video: https://youtu.be/7y8Zg87rtjs
+
+	Links
+	~~~~~
+	YouTube:	https://www.youtube.com/javidx9
+				https://www.youtube.com/javidx9extra
+	Discord:	https://discord.gg/WhwHUMV
+	Twitter:	https://www.twitter.com/javidx9
+	Twitch:		https://www.twitch.tv/javidx9
+	GitHub:		https://www.github.com/onelonecoder
+	Patreon:	https://www.patreon.com/javidx9
+	Homepage:	https://www.onelonecoder.com
+
+	Author
+	~~~~~~
+	David Barr, aka javidx9, ©OneLoneCoder 2019
+*/
+
+
+// NOTE: THIS CODE IS PROVIDED AS IS FROM THE END OF THE
+// LIVE STREAM. SO ITS A MESS. ENJOY!
+
+
+/* Alterations Joseph21 - 2022-07-18
+ *   - no changes made to the CGE version
+ * Port to PixelGameEngine - 2022-07-18
+ *   - see cheat sheet for generic adaptations needed for port
+ *   - added a function RandColour() to generate random primary colours from the PGE standard palette
+ */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <list>
+
+#include "SGE/SGE_Core.h"
+
+#undef min
+#undef max
+
+
+class Gems : public flc::SDL_GameEngine
+{
+public:
+	Gems()
+	{
+		sAppName = "50K Subscriber Gems";
+	}
+
+	struct sGem
+	{
+		flc::Pixel colour;
+		bool bExist;
+		bool bRemove;
+		bool bBomb;
+
+	};
+
+
+	enum STATES
+	{
+		STATE_USER,
+		STATE_SWAP,
+		STATE_CHECK,
+		STATE_ERASE,
+		STATE_COMPRESS,
+		STATE_NEWGEMS,
+	} nState, nNextState;
+
+	sGem gems[8][8];
+
+	float fDelayTime = 0.0f;
+	std::string sprGem, sprBomb;
+
+	int nTotalGems = 0;
+
+	int nCursorX = 0, nCursorY = 0;
+	int nSwapX = 0, nSwapY = 0;
+
+	bool bSwapFail = false;
+	bool bGemsToRemove = false;
+
+
+	struct sFragment
+	{
+		float x;
+		float y;
+		float vx;
+		float vy;
+		flc::Pixel colour;
+	};
+
+
+	std::list<sFragment> fragments;
+
+    // use only 6 primary colours
+    flc::Pixel RandColour() {
+        switch (rand() % 7) {
+            case 0: return flc::WHITE;
+            case 1: return flc::RED;
+            case 2: return flc::YELLOW;
+            case 3: return flc::GREEN;
+            case 4: return flc::BLUE;
+            case 5: return flc::MAGENTA;
+            case 6: return flc::CYAN;
+        }
+        return flc::VERY_DARK_GREY;
+    }
+
+public:
+	bool OnUserCreate()
+	{
+		sprGem += "..XXXX..";
+		sprGem += ".XXXXXX.";
+		sprGem += "XXXXXXXX";
+		sprGem += "XXX..XXX";
+		sprGem += "XXX..XXX";
+		sprGem += "XXXXXXXX";
+		sprGem += ".XXXXXX.";
+		sprGem += "..XXXX..";
+
+		sprBomb += "...XX...";
+		sprBomb += ".X.XX.X.";
+		sprBomb += "..XXXX..";
+		sprBomb += "XXXXXXXX";
+		sprBomb += "XXXXXXXX";
+		sprBomb += "..XXXX..";
+		sprBomb += ".X.XX.X.";
+		sprBomb += "...XX...";
+
+
+		for (int x = 0; x < 8; x++)
+		{
+			for (int y = 0; y < 8; y++)
+			{
+				gems[x][y].bExist = false;
+				gems[x][y].bRemove = false;
+				gems[x][y].colour = RandColour();
+			}
+		}
+
+		nState = STATE_USER;
+		nNextState = STATE_USER;
+
+
+
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime)
+	{
+		if (fDelayTime > 0.0f)
+		{
+			fDelayTime -= fElapsedTime;
+		}
+		else
+		{
+			auto boom = [&](int x, int y, int size, flc::Pixel colour)
+			{
+				auto random_float = [&](float min, float max)
+				{
+					return ((float)rand() / (float)RAND_MAX) * (max - min) + min;
+				};
+
+
+				for (int i = 0; i < size; i++)
+				{
+					float a = random_float(0, 2.0f * 3.14159f);
+					sFragment f = { (float)x, (float)y, cos(a) * random_float(30.0f, 75.0), sin(a) * random_float(30.0f, 75.0), colour };
+					fragments.push_back(f);
+				}
+
+			};
+
+
+
+			// Gameplay
+			switch (nState)
+			{
+			case STATE_USER:
+				if (nTotalGems < 64)
+					nNextState = STATE_COMPRESS;
+				else
+				{
+					if (!GetKey(flc::Key::SPACE).bHeld)
+					{
+						if (GetKey(flc::Key::LEFT).bPressed) nCursorX--;
+						if (GetKey(flc::Key::RIGHT).bPressed) nCursorX++;
+						if (GetKey(flc::Key::UP).bPressed) nCursorY--;
+						if (GetKey(flc::Key::DOWN).bPressed) nCursorY++;
+						if (nCursorX < 0) nCursorX = 0;
+						if (nCursorX > 7) nCursorX = 7;
+						if (nCursorY < 0) nCursorY = 0;
+						if (nCursorY > 7) nCursorY = 7;
+					}
+					else
+					{
+						nSwapX = nCursorX;
+						nSwapY = nCursorY;
+						if (GetKey(flc::Key::LEFT).bPressed && nCursorX > 0) nSwapX = nCursorX - 1;
+						if (GetKey(flc::Key::RIGHT).bPressed && nCursorX < 7) nSwapX = nCursorX + 1;
+						if (GetKey(flc::Key::UP).bPressed && nCursorY > 0) nSwapY = nCursorY - 1;
+						if (GetKey(flc::Key::DOWN).bPressed && nCursorY < 7) nSwapY = nCursorY + 1;
+						if (nSwapX != nCursorX || nSwapY != nCursorY) nNextState = STATE_SWAP;
+					}
+				}
+
+				break;
+			case STATE_SWAP:
+				bSwapFail = true;
+				std::swap(gems[nCursorX][nCursorY], gems[nSwapX][nSwapY]);
+				fDelayTime = 0.5f;
+				nNextState = STATE_CHECK;
+				break;
+			case STATE_CHECK:
+
+				bGemsToRemove = false;
+
+				for (int x = 0; x < 8; x++)
+				{
+					for (int y = 0; y < 8; y++)
+					{
+						if (!gems[x][y].bRemove)
+						{
+							bool bPlaceBomb = false;
+
+							// Check Horizontally
+							int nChain = 1;
+							while (gems[x][y].colour == gems[x + nChain][y].colour && (nChain + x) < 8) nChain++;
+							if (nChain >= 3)
+							{
+								if (nChain >= 4) bPlaceBomb = true;
+
+								while (nChain > 0)
+								{
+									gems[x + nChain - 1][y].bRemove = true;
+
+									if (gems[x + nChain - 1][y].bBomb)
+									{
+										for (int i = -1; i < 2; i++)
+										{
+											for (int j = -1; j < 2; j++)
+											{
+												int m = std::min(std::max(i + (x + nChain - 1), 0), 7);
+												int n = std::min(std::max(j + y, 0), 7);
+												gems[m][n].bRemove = true;
+											}
+										}
+									}
+
+									nChain--;
+									bSwapFail = false;
+									bGemsToRemove = true;
+								}
+							}
+
+
+							// Check Vertically
+							nChain = 1;
+							while (gems[x][y].colour == gems[x][y + nChain].colour && (nChain + y) < 8) nChain++;
+							if (nChain >= 3)
+							{
+								if (nChain >= 4) bPlaceBomb = true;
+
+								while (nChain > 0)
+								{
+									gems[x][y + nChain - 1].bRemove = true;
+
+
+									if (gems[x][y + nChain - 1].bBomb)
+									{
+										for (int i = -1; i < 2; i++)
+										{
+											for (int j = -1; j < 2; j++)
+											{
+												int m = std::min(std::max(i + x, 0), 7);
+												int n = std::min(std::max(j + (y + nChain - 1), 0), 7);
+												gems[m][n].bRemove = true;
+											}
+										}
+									}
+
+									nChain--;
+									bSwapFail = false;
+									bGemsToRemove = true;
+								}
+							}
+
+							if (bPlaceBomb)
+							{
+								gems[x][y].bBomb = true;
+								gems[x][y].bRemove = false;
+							}
+
+						}
+
+					}
+				}
+
+				if (bGemsToRemove)
+					fDelayTime = 0.75f;
+
+				nNextState = STATE_ERASE;
+				break;
+			case STATE_ERASE:
+				if (!bGemsToRemove)
+				{
+					if (bSwapFail)
+					{
+						std::swap(gems[nCursorX][nCursorY], gems[nSwapX][nSwapY]);
+					}
+
+					nNextState = STATE_USER;
+				}
+				else
+				{
+					for (int x = 0; x < 8; x++)
+					{
+						for (int y = 0; y < 8; y++)
+						{
+							if (gems[x][y].bRemove)
+							{
+								gems[x][y].bExist = false;
+								gems[x][y].bBomb = false;
+								boom(x * 8 + 4, y * 8 + 4, 50, gems[x][y].colour);
+								nTotalGems--;
+							}
+						}
+					}
+					nNextState = STATE_COMPRESS;
+				}
+				break;
+			case STATE_COMPRESS:
+
+				for (int y = 6; y >= 0; y--)
+				{
+					for (int x = 0; x < 8; x++)
+					{
+						if (gems[x][y].bExist && !gems[x][y + 1].bExist)
+							std::swap(gems[x][y], gems[x][y + 1]);
+					}
+				}
+
+				fDelayTime = 0.1f;
+				nNextState = STATE_NEWGEMS;
+				break;
+
+			case STATE_NEWGEMS:
+				for (int x = 0; x < 8; x++)
+				{
+					if (!gems[x][0].bExist)
+					{
+						gems[x][0].bExist = true;
+						gems[x][0].bRemove = false;
+						gems[x][0].bBomb = false;
+						gems[x][0].colour = RandColour();
+						nTotalGems++;
+					}
+				}
+
+				if (nTotalGems < 64)
+				{
+					fDelayTime = 0.1f;
+					nNextState = STATE_COMPRESS;
+				}
+				else
+					nNextState = STATE_CHECK;
+
+
+				break;
+			}
+
+
+			nState = nNextState;
+
+		} // End Gameplay
+
+
+
+
+
+		// Rendering
+		FillRect(0, 0, ScreenWidth(), ScreenHeight(), flc::BLACK);
+
+		// Draw Field
+		for (int x = 0; x < 8; x++)
+		{
+			for (int y = 0; y < 8; y++)
+			{
+				if (gems[x][y].bExist)
+				{
+					for (int i = 0; i < 8; i++)
+					{
+						for (int j = 0; j < 8; j++)
+						{
+							std::string &source = gems[x][y].bBomb ? sprBomb : sprGem;
+
+							if (source[j * 8 + i] != '.')
+								Draw(x * 8 + i, y * 8 + j, gems[x][y].colour * (gems[x][y].bRemove ? 0.5f : 0.9f));
+
+						}
+					}
+				}
+			}
+		}
+
+		// Draw Cursor
+		DrawRect(nCursorX * 8, nCursorY * 8, 7, 7, flc::WHITE);
+
+
+
+
+		for (auto &f : fragments)
+		{
+			Draw(f.x, f.y, f.colour);
+			f.x += f.vx * fElapsedTime;
+			f.y += f.vy * fElapsedTime;
+		}
+
+		std::remove_if(fragments.begin(), fragments.end(),
+		[&](const sFragment &f)
+		{
+			return f.x < 0 || f.x > ScreenWidth() || f.y < 0 || f.y > ScreenHeight();
+		});
+
+
+		return true;
+	}
+};
+
+
+int main( int argc, char *argv[] )
+{
+	Gems game;
+	if (game.Construct(64, 64, 14, 14))
+		game.Start();
+
+	return 0;
+}
